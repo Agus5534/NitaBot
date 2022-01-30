7496932c52383fc3bf1914ca31b3bce46b3d283d
CarmenInGitHub <maxi1000iano.jara@gmail.com>
2022-01-30 13:31:56 -0300

    Hola Agus

---
+++ src/main/java/com/nitasorteadora/bot/cartas/embeds/CalidadC.java
@@ -0,0 +1,10 @@
+package com.nitasorteadora.bot.cartas.embeds;
+
+public enum CalidadC {
+    COMUN,
+    RARA,
+    EPICA,
+    LEGENDARIA,
+    MITICA,
+    SECRETA;
+}
---
+++ src/main/java/com/nitasorteadora/bot/cartas/embeds/CartaC.java
@@ -0,0 +1,87 @@
+package com.nitasorteadora.bot.cartas.embeds;
+
+public enum CartaC {
+    //Comunes
+    EVIL("El Dasher","Evil","<:Evil:793945486109179904>","https://cdn.discordapp.com/attachments/818613273813319781/872308037484945438/1eldasher.jpg","CartaEvil.",1,CalidadC.COMUN),
+    MAGIC("El Modder","Magic","<:Magic:788433576848261130>","https://cdn.discordapp.com/attachments/818613273813319781/871973224005640212/1elmodder.jpg","CartaMagic.",1, CalidadC.COMUN),
+    MAURITO("El Mayor","Maurito","<:Maurito:788421660620881920>","https://cdn.discordapp.com/attachments/818613273813319781/871973273431326740/1elmayor.jpg","CartaMaurito.",1,CalidadC.COMUN),
+    SOUL("El Perdido","Soul","<:Soul:789624142739669044>","https://cdn.discordapp.com/attachments/818613273813319781/872307979532271676/1elperdido.jpg","CartaSoul.",1,CalidadC.COMUN),
+    ZEQIA("El Mudo","Zeqia","<:Soul:789624142739669044>","https://cdn.discordapp.com/attachments/818613273813319781/872201507456548936/1elmudo.jpg","CartaZeqia.",1,CalidadC.COMUN),
+    ZIJHYAN("El Mago Ingeniero","Zijhyan","<:Zijhyan:788421732125507595>","https://cdn.discordapp.com/attachments/818613273813319781/872201410341658674/1elingeniero.jpg","CartaZij.",1,CalidadC.COMUN),
+    ANDREW("El Amable","Andrew","<:Andru:821367908063117403>","https://cdn.discordapp.com/attachments/818613273813319781/873422116400283709/1elamable.jpg","CartaAndrew.",2,CalidadC.COMUN),
+    GERSOON("El Recopilador","Geerson","<:Gersoon:819332227115647036>","https://cdn.discordapp.com/attachments/818613273813319781/873350880475377735/1elrecopilador.jpg","CartaGersoon.",2,CalidadC.COMUN),
+    TAMARA1001("La Dedicada","Tamara","<:Tamara:819338319921152000>","https://cdn.discordapp.com/attachments/818613273813319781/873421903434485880/1ladedicada.jpg","CartaTamara.",2,CalidadC.COMUN),
+    XRAYOS("El Timido","XRayos","<:Xrayos:821463211001118720>","https://cdn.discordapp.com/attachments/818613273813319781/873422178471800862/1eltimido.jpg","CartaXRayos.",2,CalidadC.COMUN),
+    AYDEN("El Entusiasta","Ayden","<:Ayden:895019810449158155>","https://cdn.discordapp.com/attachments/818613273813319781/895150877919301652/1elentusiasta.jpg","CartaAyden.",3,CalidadC.COMUN),
+    CHARLYDAVID("El Multiestilo","Charly","<:Charly:864214891275223070>","https://cdn.discordapp.com/attachments/818613273813319781/879875055620874301/1elmultiestilo.jpg","CartaCharly.",3,CalidadC.COMUN),
+    FLOWII("La Cantante","Flowii","<:Flowii:864240761573998593>","https://cdn.discordapp.com/attachments/818613273813319781/884606533923639296/1lacantante.jpg","CartaFlowii.",3,CalidadC.COMUN),
+    GIANTEK("El Pelotudo","Giantek","<:Gian:866447180914884639>","https://cdn.discordapp.com/attachments/818613273813319781/884606688206925864/1elpelotudo.jpg","CartaGiantek.",3,CalidadC.COMUN),
+    GOGETA1956("El Referencias","Goge","<:Goge:864242052559667210>","https://cdn.discordapp.com/attachments/818613273813319781/884606642707103814/1elreferencias.jpg","CartaGoge.",3,CalidadC.COMUN),
+    JUUZO("La Inexperta","Juzo","<:Juzo:864242052514578482>","https://cdn.discordapp.com/attachments/818613273813319781/884606570997108746/1lainexperta.jpg","CartaJuzo.",3,CalidadC.COMUN),
+    MRCOMEGATOS("El Hayabusa","MrComeGatos","<:MrComeGatos:864214891396726804>","https://cdn.discordapp.com/attachments/818613273813319781/884575170243747880/1elhayabusa.jpg","CartaMrComeGatos.",3,CalidadC.COMUN),
+    SPIKER("El Boomer","Spiker","<:Spiker:864214891458986034>","https://cdn.discordapp.com/attachments/818613273813319781/884575316805308436/1elboomer.jpg","CartaSpiker.",3,CalidadC.COMUN),
+    SRLEXAN("El Distraido","Lexan","<:Lexan:892154822277619722>","https://cdn.discordapp.com/attachments/818613273813319781/892875761189519411/1eldistraido.jpg","CartaLexan.",3,CalidadC.COMUN),
+    STARRFALLZ("La Indecisa","Starr","<:Starrzfall:864216141685456956>","https://cdn.discordapp.com/attachments/818613273813319781/884575359788535869/1laindecisa.jpg","CartaStarr.",3,CalidadC.COMUN),
+    TATURC("El Boludo","Tatu","<:Tatu:864214891249139772>","https://cdn.discordapp.com/attachments/818613273813319781/879509809580367902/1elboludo.jpg","CartaTatu.",3,CalidadC.COMUN),
+
+    //Raras
+    CNEYER("El Aplanador","Cneyer","<:Cneyer:800064014381482004>","https://cdn.discordapp.com/attachments/818613273813319781/872308109375340554/1elaplanador.jpg","CartaCneyer.",1,CalidadC.RARA),
+    CUCHI("La Pequeña","Cuchi","<:Cuchi:864245161993961473>","https://cdn.discordapp.com/attachments/818613273813319781/871923636737564693/1lapequena.jpg","CartaCuchi.",1,CalidadC.RARA),
+    UIMPI("La Animadora","Uimpi","<:Uimpi:799828322287747092>","https://cdn.discordapp.com/attachments/818613273813319781/872201333636222976/1laanimadora.jpg","CartaUimpi.",1,CalidadC.RARA),
+    JUNGLE("El Humilde","Jungle","<:Jungle:834510259758301284>","https://cdn.discordapp.com/attachments/818613273813319781/879509650477842492/1elhumilde.jpg","CartaJungle.",2,CalidadC.RARA),
+    PATO("El Discreto","Pato","<:Pato:822562210554445845>","https://cdn.discordapp.com/attachments/818613273813319781/873422071340884029/1eldiscreto.jpg","CartaPato.",2,CalidadC.RARA),
+    RUFK("El Inquieto","Rufk","<:Rufk:819332353939210241>","https://cdn.discordapp.com/attachments/818613273813319781/873422021311221800/1elinquieto.jpg","CartaRufk.",2,CalidadC.RARA),
+    SALVADOR("El Impaciente","Salvi","<:Salvador:819332288759332864>","https://cdn.discordapp.com/attachments/818613273813319781/873350979045711943/1elimpaciente.jpg","CartaSalvi.",2,CalidadC.RARA),
+    AZGALORD("La Tele","Azga","<:Azgalord:879875183572287518>","https://cdn.discordapp.com/attachments/818613273813319781/879874891472588860/1latele.jpg","CartaAzga.",3,CalidadC.RARA),
+    HEIS("El Sicario","Heis","<:Heis:864214891493326878>","https://cdn.discordapp.com/attachments/818613273813319781/879509720266862662/1elsicario.jpg","CartaHeis.",3,CalidadC.RARA),
+    INFERNAL("El Loco","Infernal","<:Infernal:864214891149262859>","https://cdn.discordapp.com/attachments/818613273813319781/879874620407300096/1elloco.jpg","CartaInfer.",3,CalidadC.RARA),
+    MARIO("El Pokemaníaco","Mario","<:Mario:864214891283349505>","https://cdn.discordapp.com/attachments/818613273813319781/884575253915926588/1elpokemaniaco.jpg","CartaMario.",3,CalidadC.RARA),
+    YURI("El Valerense","Yuri","<:Yuri:864214891538677780>","https://cdn.discordapp.com/attachments/818613273813319781/879874807427104768/elvarelense.jpg","CartaYuri.",3,CalidadC.RARA),
+
+    //Epicas
+    AUGUSIMPERIAL("El Imperial","AugusImperial","<:Augus:804429263431532605>","https://cdn.discordapp.com/attachments/818613273813319781/871972935185879070/1elimperial.jpg","CartaAugus.",1,CalidadC.EPICA),
+    CELESTIAL("El Escandaloso","Celestial","<:Celestial:795497092755095582>","https://cdn.discordapp.com/attachments/818613273813319781/872308160877178890/1elescandaloso.jpg","CartaCel.",1,CalidadC.EPICA),
+    IKI("El Guardián","Iki","<:Iki:830295031319101471>","https://cdn.discordapp.com/attachments/818613273813319781/872201470202753065/1elguardian.jpg","CartaIki.",1,CalidadC.EPICA),
+    IVOO("El Turro","Ivoo","<:Ivo:788418435590914058>","https://cdn.discordapp.com/attachments/818613273813319781/872201224819200080/1elturro.jpg","CartaIvo.",1,CalidadC.EPICA),
+    JUANJO("El Técnico","Juanjo","<:Juanjo:788418503098630164>","https://cdn.discordapp.com/attachments/818613273813319781/873350840679809034/1eltecnico.jpg","CartaJuanjo.",1,CalidadC.EPICA),
+    LUQUIMC("El Tranquilo","LuquiMC","<:Luqui:851185434284916787>","https://cdn.discordapp.com/attachments/818613273813319781/871973173573324840/1eltranquilo.jpg","CartaLuqui.",1,CalidadC.EPICA),
+    NITA("La Artista","Nita","<:Nita:864245827891363871>","https://cdn.discordapp.com/attachments/818613273813319781/871923529573105714/1laartista.jpg","CartaNita.",1,CalidadC.EPICA),
+    SUBXASO("El Comediante","SubXaso","<:Sub:871851399628079184>","https://cdn.discordapp.com/attachments/818613273813319781/871923679825649664/1elcomediante.jpg","CartaSub.",1,CalidadC.EPICA),
+    THEBOYPANDA("El Paciente","TheBoyPanda","<:Panda:871861308251856936>","https://cdn.discordapp.com/attachments/818613273813319781/871923579388821564/1elpaciente.jpg","CartaPanda.",1,CalidadC.EPICA),
+    CONNG("La Editora","ConnG","<:Conn:852972434188795915>","https://cdn.discordapp.com/attachments/818613273813319781/873351022221856768/1laeditora.jpg","CartaConn.",2,CalidadC.EPICA),
+
+    //Legendarias
+    LUCMUS("El Admin","Lucmus","<:Lucmus:867887798476472331>","https://cdn.discordapp.com/attachments/818613273813319781/871923482953392128/1eladmin.jpg","CartaLucmus.",1,CalidadC.LEGENDARIA),
+    NYREM("El Callado","Nyrem","<:Nyrem:842794066299191296>","https://cdn.discordapp.com/attachments/818613273813319781/871973314631970816/1elcallado.jpg","CartaNyrem.",1,CalidadC.LEGENDARIA),
+    AGUS("El Programador","Agus5534","<:Agus:873056466972581920>","https://cdn.discordapp.com/attachments/818613273813319781/884650845638103050/1elprogramador.jpg","CartaAgus5534.",3,CalidadC.LEGENDARIA),
+
+    //Miticas
+    LaEntidad("La Entidad","LaEntidad","<:testemote:892895101527932991>","https://cdn.discordapp.com/attachments/818995908779835393/892876921438875648/1entidad.jpg","CartaLaEntidad.",3,CalidadC.MITICA),
+    NitaBot("NitaBot","NitaBot","<:testemote:892895101527932991>","https://cdn.discordapp.com/attachments/818995908779835393/892876959032414218/1nitabot.jpg","CartaNitaBot.",3,CalidadC.MITICA);
+
+
+    private String name;
+    private String author;
+    private String emote;
+    private String image;
+    private String dbName;
+    private int season;
+    private CalidadC calidad;
+
+    CartaC(String name, String author, String emote, String image, String dbName, int season, CalidadC calidad){
+        this.name = name;
+        this.author = author;
+        this.emote = emote;
+        this.image = image;
+        this.dbName = dbName;
+        this.season = season;
+        this.calidad = calidad;
+    }
+    public String getName() { return name; }
+    public String getAuthor() { return author; }
+    public String getEmote() { return emote; }
+    public String getImage() { return image; }
+    public String getDBName() { return dbName; }
+    public Integer getSeason() { return season; }
+    public CalidadC getCalidad() { return calidad; }
+}
