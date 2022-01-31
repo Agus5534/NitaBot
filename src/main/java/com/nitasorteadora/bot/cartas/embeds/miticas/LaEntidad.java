6b863eb93acc6f3cfcdf9dd7c0079e1139d4c1c3
Agus5534 <agustinmonte06@gmail.com>
2022-01-31 17:29:50 -0300

    Migrated from secrets cards to mythicals

--- src/main/java/com/nitasorteadora/bot/cartas/embeds/miticas/LaEntidad.java
+++ src/main/java/com/nitasorteadora/bot/cartas/embeds/miticas/LaEntidad.java
@@ -2,43 +2,28 @@ package com.nitasorteadora.bot.cartas.embeds.miticas;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.nitasorteadora.bot.cartas.config.EmbedHelper;
+import com.nitasorteadora.bot.utils.cartashelper.CartasHelper;
+import com.nitasorteadora.bot.utils.exceptions.CreateCardException;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 import static com.nitasorteadora.bot.Main.cartas;
 import static com.nitasorteadora.bot.Main.cartasconfig;
 import static com.nitasorteadora.bot.utils.lucksystem.LuckManager.resetLuck;
 
-public class LaEntidad implements EmbedHelper {
+public class LaEntidad {
     public static void Entidad(CommandEvent commandEvent) {
-        int cantcartas = 0;
-        int total = 0;
+        CartasHelper carta = new CartasHelper(commandEvent);
+        carta.cardName("La Entidad")
+                .author("La Entidad")
+                .emote("<:testemote:892895101527932991>")
+                .image("https://cdn.discordapp.com/attachments/818995908779835393/892876921438875648/1entidad.jpg")
+                .dbName("CartaLaEntidad.")
+                .season(3)
+                .esMitica();
         try {
-            cantcartas = (int) cartasconfig.get("CartaLaEntidad." + commandEvent.getAuthor().getId(), cantcartas);
-            total = (int) cartasconfig.get("Total." + commandEvent.getAuthor().getId(), total);
-            total++;
-            cartasconfig.set("Total."+commandEvent.getAuthor().getId(),total);
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setTitle("¡Carta diaria!");
-            eb.setDescription("Obtuviste: **La Entidad** <:testemote:892895101527932991>");
-            eb.setImage("https://cdn.discordapp.com/attachments/818995908779835393/892876921438875648/1entidad.jpg");
-            eb.setFooter(secretas3footer + " | Carta hecha por Zazil");
-            eb.setColor(colorsecreto);
-            commandEvent.getChannel().sendMessage(commandEvent.getAuthor().getAsMention() + " Y... You... Just.... NOOOOOOOOOOOOOOOOOOOO!").queue();
-            commandEvent.getChannel().sendMessageEmbeds(eb.build()).queue();
-            cantcartas++;
-            cartasconfig.set("CartaLaEntidad." + commandEvent.getAuthor().getId(), cantcartas);
-            int desbloqueadas = 0;
-            if(cantcartas == 1) {
-                desbloqueadas = (int) cartasconfig.get("Desbloqueadas." + commandEvent.getAuthor().getId(), desbloqueadas);
-                desbloqueadas++;
-                cartasconfig.set("Desbloqueadas."+commandEvent.getAuthor().getId(),desbloqueadas);
-            }
-            cartasconfig.save(cartas);
-            resetLuck(commandEvent);
-        } catch (Exception e) {
-            cartasconfig.set("Total." + commandEvent.getAuthor().getId(), total);
-            cartasconfig.set("CartaLaEntidad." + commandEvent.getAuthor().getId(), cantcartas);
-            commandEvent.getChannel().sendMessage(commandEvent.getAuthor().getAsMention() + " agregando a la base de datos... (Vuelve a utilizar el comando)");
+            carta.build(commandEvent);
+        } catch (CreateCardException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/nitasorteadora/bot/cartas/embeds/miticas/NitaBot.java
+++ src/main/java/com/nitasorteadora/bot/cartas/embeds/miticas/NitaBot.java
@@ -2,43 +2,28 @@ package com.nitasorteadora.bot.cartas.embeds.miticas;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.nitasorteadora.bot.cartas.config.EmbedHelper;
+import com.nitasorteadora.bot.utils.cartashelper.CartasHelper;
+import com.nitasorteadora.bot.utils.exceptions.CreateCardException;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 import static com.nitasorteadora.bot.Main.cartas;
 import static com.nitasorteadora.bot.Main.cartasconfig;
 import static com.nitasorteadora.bot.utils.lucksystem.LuckManager.resetLuck;
 
-public class NitaBot implements EmbedHelper {
+public class NitaBot {
     public static void NitaBot(CommandEvent commandEvent) {
-        int cantcartas = 0;
-        int total = 0;
+        CartasHelper carta = new CartasHelper(commandEvent);
+        carta.cardName("NitaBot")
+                .author("NitaBot")
+                .emote("<:testemote:892895101527932991>")
+                .image("https://cdn.discordapp.com/attachments/818995908779835393/892876959032414218/1nitabot.jpg")
+                .dbName("CartaNitaBot.")
+                .season(3)
+                .esMitica();
         try {
-            cantcartas = (int) cartasconfig.get("CartaNitaBot." + commandEvent.getAuthor().getId(), cantcartas);
-            total = (int) cartasconfig.get("Total." + commandEvent.getAuthor().getId(), total);
-            total++;
-            cartasconfig.set("Total."+commandEvent.getAuthor().getId(),total);
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setTitle("¡Carta diaria!");
-            eb.setDescription("Obtuviste: **NitaBot** <:testemote:892895101527932991>");
-            eb.setImage("https://cdn.discordapp.com/attachments/818995908779835393/892876959032414218/1nitabot.jpg");
-            eb.setFooter(secretas3footer);
-            eb.setColor(colorsecreto);
-            commandEvent.getChannel().sendMessage(commandEvent.getAuthor().getAsMention() + " Hola! Me presento, soy Nita Sorteadora, un bot que fue creado por Panda, Nita y Agus5534, desarrollado por este último. Te cuento un poco sobre mí, un día, en el servidor de Discord de Lucmus, en el canal de staff, Lucmus le preguntó a Agus sobre lo de hacer bots porque necesitaba uno para sortear teams para el UHC Weekend. Agus aceptó y así nací... Tiempo después, Panda y Nita tenian ganas de hacer que las cartas de cada personaje, que estaban recien salidas, salgan en el bot mediante azar. Asi llegamos a hoy en día!").queue();
-            commandEvent.getChannel().sendMessageEmbeds(eb.build()).queue();
-            cantcartas++;
-            cartasconfig.set("CartaNitaBot." + commandEvent.getAuthor().getId(), cantcartas);
-            int desbloqueadas = 0;
-            if(cantcartas == 1) {
-                desbloqueadas = (int) cartasconfig.get("Desbloqueadas." + commandEvent.getAuthor().getId(), desbloqueadas);
-                desbloqueadas++;
-                cartasconfig.set("Desbloqueadas."+commandEvent.getAuthor().getId(),desbloqueadas);
-            }
-            cartasconfig.save(cartas);
-            resetLuck(commandEvent);
-        } catch (Exception e) {
-            cartasconfig.set("Total." + commandEvent.getAuthor().getId(), total);
-            cartasconfig.set("CartaNitaBot." + commandEvent.getAuthor().getId(), cantcartas);
-            commandEvent.getChannel().sendMessage(commandEvent.getAuthor().getAsMention() + " agregando a la base de datos... (Vuelve a utilizar el comando)").queue();
+            carta.build(commandEvent);
+        } catch (CreateCardException e) {
+            e.printStackTrace();
         }
     }
 }
