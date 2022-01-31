e03c612892a520ac4ada86f2db1a2e41adea3545
CarmenInGitHub <maxi1000iano.jara@gmail.com>
2022-01-31 20:27:09 -0300

    Hola agus como estas

---
+++ src/main/java/com/nitasorteadora/bot/utils/UnlockedChecker.java
@@ -0,0 +1,92 @@
+package com.nitasorteadora.bot.utils;
+
+public class UnlockedChecker {
+
+    private void checker() {
+        int cevil = 0;
+        int cmagic = 0;
+        int cmaurito = 0;
+        int czij = 0;
+        int ccuchi = 0;
+        int cuimpi = 0;
+        int caugus = 0;
+        int ciki = 0;
+        int civoo = 0;
+        int cluqui = 0;
+        int cnita = 0;
+        int csub = 0;
+        int cpanda = 0;
+        int clucmus = 0;
+        int cnyrem = 0;
+        int cpato = 0;
+        int crufk = 0;
+        int csalvi = 0;
+        int cconn = 0;
+        int ccharly = 0;
+        int cflowii = 0;
+        int cgoge = 0;
+        int cspiker = 0;
+        int clexan = 0;
+        int cstarr = 0;
+        int ctatu = 0;
+        int cazga = 0;
+        int cheis = 0;
+        int cinfer = 0;
+        int cmario = 0;
+        int cyuri = 0;
+        int cagus5534 = 0;
+        int cayden = 0;
+        int laentidad = 0;
+        int nitabot = 0;
+        int cfabo = 0;
+        int ccarmened = 0;
+        int desbloqueadas = 0;
+
+         if(cevil >= 1){desbloqueadas++;}
+         if(cmagic >= 1){desbloqueadas++;}
+         if(cmaurito >= 1){desbloqueadas++;}
+         if(czij >= 1){desbloqueadas++;}
+         if(ccuchi >= 1){desbloqueadas++;}
+         if(cuimpi >= 1){desbloqueadas++;}
+         if(caugus >= 1){desbloqueadas++;}
+         if(ciki >= 1){desbloqueadas++;}
+         if(civoo >= 1){desbloqueadas++;}
+         if(cluqui >= 1){desbloqueadas++;}
+        if(cnita>= 1){desbloqueadas++;}
+        if(csub >= 1){desbloqueadas++;}
+        if(cpanda >= 1){desbloqueadas++;}
+        if(clucmus >= 1){desbloqueadas++;}
+        if(cnyrem >= 1){desbloqueadas++;}
+        if(cpato >= 1){desbloqueadas++;}
+        if(crufk >= 1){desbloqueadas++;}
+        if(csalvi >= 1){desbloqueadas++;}
+        if(cconn >= 1){desbloqueadas++;}
+        if(ccharly >= 1){desbloqueadas++;}
+        if(cflowii >= 1){desbloqueadas++;}
+        if(cgoge >= 1){desbloqueadas++;}
+        if(cspiker >= 1){desbloqueadas++;}
+        if(clexan >= 1){desbloqueadas++;}
+        if(cstarr >= 1){desbloqueadas++;}
+        if(ctatu >= 1){desbloqueadas++;}
+        if(cazga >= 1){desbloqueadas++;}
+        if(cheis >= 1){desbloqueadas++;}
+        if(cinfer >= 1){desbloqueadas++;}
+        if(cmario >= 1){desbloqueadas++;}
+        if(cyuri >= 1){desbloqueadas++;}
+        if(cagus5534 >= 1){desbloqueadas++;}
+        if(cayden >= 1){desbloqueadas++;}
+        if(laentidad >= 1){desbloqueadas++;}
+        if(nitabot >= 1){desbloqueadas++;}
+        if(cfabo >= 1){desbloqueadas++;}
+        if(ccarmened >= 1){desbloqueadas++;}
+
+
+
+
+
+
+
+
+
+    }
+}
