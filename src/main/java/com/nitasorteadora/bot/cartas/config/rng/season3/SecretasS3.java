49ed055d2cd70eb7ab867ffc10bb096a40fd963b
Agus5534 <agustinmonte06@gmail.com>
2022-01-31 17:30:00 -0300

    Bug Fix

--- src/main/java/com/nitasorteadora/bot/cartas/config/rng/season3/SecretasS3.java
+++ src/main/java/com/nitasorteadora/bot/cartas/config/rng/season3/SecretasS3.java
@@ -1,8 +1,8 @@
 package com.nitasorteadora.bot.cartas.config.rng.season3;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
-import com.nitasorteadora.bot.cartas.embeds.secretas.season3.LaEntidad;
-import com.nitasorteadora.bot.cartas.embeds.secretas.season3.NitaBot;
+import com.nitasorteadora.bot.cartas.embeds.miticas.LaEntidad;
+import com.nitasorteadora.bot.cartas.embeds.miticas.NitaBot;
 
 public class SecretasS3 {
     public static void SecrS3(CommandEvent commandEvent) {
