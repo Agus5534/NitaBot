c366ebe53232bae656419e7a6ff7252da5678cea
Agus5534 <agustinmonte06@gmail.com>
2022-03-11 16:18:38 -0300

    Updated not verified warning delay

--- src/main/java/com/nitasorteadora/bot/minecraft/events/JoinEvent.java
+++ src/main/java/com/nitasorteadora/bot/minecraft/events/JoinEvent.java
@@ -23,7 +23,7 @@ public class JoinEvent implements Listener {
             Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
                 player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1,0.5f);
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c[Nita Bot] &bNo estás verificado! Para verificar ve a comandos en el discord y pon &6w!link " + player.getName()));
-            }, 280L);
+            }, 1200L);
         }
     }
 }
